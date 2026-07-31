0da6d5ddaad026194de652066434476b86ec1d60
Samantha Placito <smelendrez002@gmail.com>
2026-07-30 17:39:58 -0700

    Modified project

--- src/client.js
+++ src/client.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const URL = 'https://kxleqziyqxfsnbtfpxug.supabase.co'
-const API_KEY = 'sb_publishable_EwwtsJ4RkbO-K2hxRx5eBA_yH86pFmg'
+const URL = import.meta.env.VITE_SUPABASE_URL
+const API_KEY = import.meta.env.VITE_SUPABASE_KEY
 
 export const supabase = createClient(URL, API_KEY)
